422a4b4f5b399a8616bec12ac731a92fa30d0515
cristianriera <7criera@stpatrick.org>
2016-11-03 11:25:19 -0500

    Add files via upload
    Added to color intensity

--- color.js
+++ color.js
@@ -1,5 +1,5 @@
 //Declare the Color Object with our new keyword below here.
-Const Color = new Object()
+Const Color = new Object();
 
 /*
 ** rgb is an array that hold the color values.
@@ -23,14 +23,22 @@ Color.isColor = function(rgb){
 };
 
 Color.redIntensity = function(rgb){
-
+  let redworks = rgb[0]/255 * 100;
+  Math.round();
+  return redworks;
 };
 
-//greenIntensity
+Color.greenIntensity = function(rgb){
+  let greenworks = rgb[1]/255 * 100;
+  Math.round();
+  return greenworks;
+};
 
-
-//blueIntensity
-
+Color.blueIntensity = function(rgb){
+  let blueworks = rgb[2]/255 * 100;
+  Math.round();
+  return blueworks;
+};
 
 //brightness
 
